afd00de52e7fbcc4a5b7f184d6786a9aec67edee
Lerry William <ghostspell89@yahoo.com>
2019-04-08 16:57:47 +0800

    Create watermark.py

---
+++ watermark.py
@@ -0,0 +1,30 @@
+"""
+Created on Mon Apr  8 11:09:29 2019
+
+@author: lerryw
+@description: Create watermark for map tiles.
+"""
+
+import os
+from PIL import Image
+
+#Note:Path should be directed to zoom level folder.
+zoomlevel_path = r"./images"
+lgo = r"lerryws_watermark.png"
+
+logo = Image.open(lgo)
+logoWidth = int(logo.width)
+logoHeight = int(logo.height)
+
+for root, dirs, files in os.walk(zoomlevel_path):
+    for directory in dirs:
+        folder_path = zoomlevel_path+'/'+directory
+        for filename in os.listdir(folder_path):
+            if (filename.endswith('.jpg') or filename.endswith('.png')):
+                image = Image.open(folder_path + '/' + filename)
+                imageWidth = int(image.width)
+                imageHeight = int(image.height)
+                image.paste(logo, (int((imageWidth - logoWidth)*0.5), int((imageHeight - logoHeight)*0.5)), logo)
+                image.save(folder_path + '/' + filename)
+
+print("Successful!")
