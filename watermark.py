92cfedb249ee92ed2072ffaf7dce7fe24b0bde84
Lerry William <ghostspell89@yahoo.com>
2019-04-09 11:00:34 +0800

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -1,30 +1,45 @@
+# -*- coding: utf-8 -*-
 """
-Created on Mon Apr  8 11:09:29 2019
+Created on Tue Apr  9 10:25:23 2019
 
 @author: lerryw
-@description: Create watermark for map tiles.
 """
 
 import os
 from PIL import Image
 
-#Note:Path should be directed to zoom level folder.
-zoomlevel_path = r"/to/zoomlevel/folder/"
-lgo = r"your_beautiful_logo_or_watermark.png"
+'''
+NOTE: Map tiles dimesion usually 256 X 256 px, so recommended watermark should be smaller than that.
+'''
 
-logo = Image.open(lgo)
+#Note:Path should be directed to tile folder.
+tiles_path = r"C:/xampp/htdocs/LeafletMBTiles/terrain2" #directory for tiles folder
+logo = Image.open(r"logo.png") #directory to logo or watermark
 logoWidth = int(logo.width)
 logoHeight = int(logo.height)
 
-for root, dirs, files in os.walk(zoomlevel_path):
+for root, dirs, files in os.walk(tiles_path):
     for directory in dirs:
-        folder_path = zoomlevel_path+'/'+directory
-        for filename in os.listdir(folder_path):
-            if (filename.endswith('.jpg') or filename.endswith('.png')):
-                image = Image.open(folder_path + '/' + filename)
-                imageWidth = int(image.width)
-                imageHeight = int(image.height)
-                image.paste(logo, (int((imageWidth - logoWidth)*0.5), int((imageHeight - logoHeight)*0.5)), logo)
-                image.save(folder_path + '/' + filename)
+        folder_path = tiles_path + '/' + directory
+        for fd_root, fd_dirs, fd_files in os.walk(folder_path):
+            for fd_directory in fd_dirs:
+                fd_path = folder_path + '/' + fd_directory
+                for filename in os.listdir(fd_path):
+                    if (filename.endswith('.jpg') or filename.endswith('.png')):
+                        image = Image.open(fd_path + '/' + filename)
+                        #print(fd_path + '/' + filename)
+                        imageWidth = int(image.width)
+                        imageHeight = int(image.height)
+                        
+                        #position
+                        topleft = 0,0
+                        topright = int(imageWidth - logoWidth),0
+                        bottomleft = 0,int(imageHeight - logoHeight)
+                        buttomright = int(imageWidth - logoWidth), int(imageHeight - logoHeight)
+                        center = int((imageWidth - logoWidth)*0.5), int((imageHeight - logoHeight)*0.5)
+                        
+                        image.paste(logo, topleft, logo)
+                        image.save(fd_path + '/' + filename)
+                        print("Added watermark to " + fd_path + '/' + filename)
 
 print("Successful!")
